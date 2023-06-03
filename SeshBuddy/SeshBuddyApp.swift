65b3c4c9f53418da183c1f00394058140eed156a
Jake Gibbons <jakegibbons@jgconsulting.services>
2023-06-03 04:10:02 +0100

    Initial Commit

---
+++ SeshBuddy/SeshBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeshBuddyApp.swift
+//  SeshBuddy
+//
+//  Created by Jake Gibbons on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SeshBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
